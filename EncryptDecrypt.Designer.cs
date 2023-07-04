c6ee4513e288584d6fe3d970b15e52740f010c2f
Izzat Ariz <izzatariz.2000@gmail.com>
2023-07-04 18:02:03 +0800

    first commit

---
+++ EncryptDecrypt.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace EncryptDecryptProgram
+{
+    partial class EncryptDecrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblText = new Label();
+            txtBoxMainText = new TextBox();
+            lblKey = new Label();
+            txtBoxKey = new TextBox();
+            gBoxEncrypt = new GroupBox();
+            txtBoxEncrypt = new TextBox();
+            gBoxDecrypt = new GroupBox();
+            txtBoxDecrypt = new TextBox();
+            btnEncrypt = new Button();
+            btnDecrypt = new Button();
+            btnReset = new Button();
+            gBoxEncrypt.SuspendLayout();
+            gBoxDecrypt.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblText
+            // 
+            lblText.AutoSize = true;
+            lblText.Location = new Point(34, 50);
+            lblText.Name = "lblText";
+            lblText.Size = new Size(28, 15);
+            lblText.TabIndex = 0;
+            lblText.Text = "Text";
+            // 
+            // txtBoxMainText
+            // 
+            txtBoxMainText.Location = new Point(97, 47);
+            txtBoxMainText.Name = "txtBoxMainText";
+            txtBoxMainText.Size = new Size(428, 23);
+            txtBoxMainText.TabIndex = 1;
+            // 
+            // lblKey
+            // 
+            lblKey.AutoSize = true;
+            lblKey.Location = new Point(34, 112);
+            lblKey.Name = "lblKey";
+            lblKey.Size = new Size(26, 15);
+            lblKey.TabIndex = 2;
+            lblKey.Text = "Key";
+            // 
+            // txtBoxKey
+            // 
+            txtBoxKey.Location = new Point(97, 109);
+            txtBoxKey.Name = "txtBoxKey";
+            txtBoxKey.Size = new Size(428, 23);
+            txtBoxKey.TabIndex = 3;
+            // 
+            // gBoxEncrypt
+            // 
+            gBoxEncrypt.Controls.Add(txtBoxEncrypt);
+            gBoxEncrypt.Location = new Point(97, 217);
+            gBoxEncrypt.Name = "gBoxEncrypt";
+            gBoxEncrypt.Size = new Size(428, 185);
+            gBoxEncrypt.TabIndex = 4;
+            gBoxEncrypt.TabStop = false;
+            gBoxEncrypt.Text = "Encrypt";
+            // 
+            // txtBoxEncrypt
+            // 
+            txtBoxEncrypt.Location = new Point(15, 35);
+            txtBoxEncrypt.Multiline = true;
+            txtBoxEncrypt.Name = "txtBoxEncrypt";
+            txtBoxEncrypt.Size = new Size(391, 132);
+            txtBoxEncrypt.TabIndex = 0;
+            // 
+            // gBoxDecrypt
+            // 
+            gBoxDecrypt.Controls.Add(txtBoxDecrypt);
+            gBoxDecrypt.Location = new Point(97, 505);
+            gBoxDecrypt.Name = "gBoxDecrypt";
+            gBoxDecrypt.Size = new Size(428, 185);
+            gBoxDecrypt.TabIndex = 5;
+            gBoxDecrypt.TabStop = false;
+            gBoxDecrypt.Text = "Decrypt";
+            // 
+            // txtBoxDecrypt
+            // 
+            txtBoxDecrypt.Location = new Point(15, 33);
+            txtBoxDecrypt.Multiline = true;
+            txtBoxDecrypt.Name = "txtBoxDecrypt";
+            txtBoxDecrypt.Size = new Size(391, 132);
+            txtBoxDecrypt.TabIndex = 1;
+            // 
+            // btnEncrypt
+            // 
+            btnEncrypt.BackColor = Color.PaleGreen;
+            btnEncrypt.Location = new Point(242, 158);
+            btnEncrypt.Name = "btnEncrypt";
+            btnEncrypt.Size = new Size(96, 35);
+            btnEncrypt.TabIndex = 6;
+            btnEncrypt.Text = "ENCRYPT";
+            btnEncrypt.UseVisualStyleBackColor = false;
+            btnEncrypt.Click += btnEncrypt_Click;
+            // 
+            // btnDecrypt
+            // 
+            btnDecrypt.BackColor = Color.LightPink;
+            btnDecrypt.Location = new Point(159, 435);
+            btnDecrypt.Name = "btnDecrypt";
+            btnDecrypt.Size = new Size(96, 35);
+            btnDecrypt.TabIndex = 7;
+            btnDecrypt.Text = "DECRYPT";
+            btnDecrypt.UseVisualStyleBackColor = false;
+            btnDecrypt.Click += btnDecrypt_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.BackColor = Color.Crimson;
+            btnReset.Location = new Point(324, 435);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(96, 35);
+            btnReset.TabIndex = 8;
+            btnReset.Text = "RESET";
+            btnReset.UseVisualStyleBackColor = false;
+            btnReset.Click += btnReset_Click;
+            // 
+            // EncryptDecrypt
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 711);
+            Controls.Add(btnReset);
+            Controls.Add(btnDecrypt);
+            Controls.Add(btnEncrypt);
+            Controls.Add(gBoxDecrypt);
+            Controls.Add(gBoxEncrypt);
+            Controls.Add(txtBoxKey);
+            Controls.Add(lblKey);
+            Controls.Add(txtBoxMainText);
+            Controls.Add(lblText);
+            Name = "EncryptDecrypt";
+            Text = "Encrypt Decrypt Program";
+            gBoxEncrypt.ResumeLayout(false);
+            gBoxEncrypt.PerformLayout();
+            gBoxDecrypt.ResumeLayout(false);
+            gBoxDecrypt.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblText;
+        private TextBox txtBoxMainText;
+        private Label lblKey;
+        private TextBox txtBoxKey;
+        private GroupBox gBoxEncrypt;
+        private GroupBox gBoxDecrypt;
+        private Button btnEncrypt;
+        private TextBox txtBoxEncrypt;
+        private TextBox txtBoxDecrypt;
+        private Button btnDecrypt;
+        private Button btnReset;
+    }
+}
---
+++ EncryptDecrypt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptDecryptProgram
+{
+    public partial class EncryptDecrypt : Form
+    {
+        public EncryptDecrypt()
+        {
+            InitializeComponent();
+            this.Text = "Encrypt Decrypt Program";
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // Get the input string and key
+            string input = txtBoxMainText.Text;
+            string key = txtBoxKey.Text;
+
+            // Encrypt the input string
+            string encrypted = Encrypt(input, key);
+
+            // Display the encrypted string in the output text box
+            txtBoxEncrypt.Text = encrypted;
+        }
+
+        private static string Encrypt(string input, string key)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] keyBytes = new byte[32];
+            Array.Copy(Encoding.UTF8.GetBytes(key), keyBytes, Math.Min(32, key.Length));
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = keyBytes;
+                aes.GenerateIV();
+                using (ICryptoTransform encryptor = aes.CreateEncryptor())
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        ms.Write(aes.IV, 0, aes.IV.Length);
+                        using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                        {
+                            cs.Write(inputBytes, 0, inputBytes.Length);
+                            cs.FlushFinalBlock();
+                            byte[] encryptedBytes = ms.ToArray();
+                            return Convert.ToBase64String(encryptedBytes);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            // Get the input string and key
+            string input = txtBoxEncrypt.Text;
+            string key = txtBoxKey.Text;
+
+            // Decrypt the input string
+            string decrypted = Decrypt(input, key);
+
+            // Display the decrypted string in the output text box
+            txtBoxDecrypt.Text = decrypted;
+        }
+
+        private static string Decrypt(string input, string key)
+        {
+            byte[] inputBytes = Convert.FromBase64String(input);
+            byte[] keyBytes = new byte[32];
+            Array.Copy(Encoding.UTF8.GetBytes(key), keyBytes, Math.Min(32, key.Length));
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = keyBytes;
+                using (MemoryStream ms = new MemoryStream(inputBytes))
+                {
+                    byte[] iv = new byte[aes.IV.Length];
+                    ms.Read(iv, 0, iv.Length);
+                    aes.IV = iv;
+                    using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                        {
+                            byte[] decryptedBytes = new byte[inputBytes.Length];
+                            int byteCount = cs.Read(decryptedBytes, 0, decryptedBytes.Length);
+                            return Encoding.UTF8.GetString(decryptedBytes, 0, byteCount);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBoxDecrypt.Clear();
+            txtBoxEncrypt.Clear();
+            txtBoxKey.Clear();
+            txtBoxMainText.Clear();
+        }
+    }
+}
